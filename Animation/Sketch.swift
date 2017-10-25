51252267eead3a6f2823ed9aeb464f48be26c7ac
Dylan Becker <dbecker@lcs-mba3263.lcs.on.ca>
2017-10-25 10:25:24 -0400

    Made the circle to bounce from left to right

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,6 +9,9 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     
+    // Change is position
+    var dx : Int        // difference in x
+    
     // This function runs once
     override init() {
         
@@ -16,15 +19,38 @@ class Sketch : NSObject {
         canvas = Canvas(width: 500, height: 500)
         
         // Set starting position
-        x = 250
+        x = 0
+        
+        
+    // Set the difference for x
+        dx = 3
         
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
+        // Clear the background
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
+        
+        
+        
+        canvas.fillColor = Color.black
         // Change position
-        x += 1
+        x += dx
+        
+        // Check position and reverse course
+        // If we go off the right edge of the screen
+        if x > 475 {
+            dx = -3
+        }
+       // Check position and reverse course
+        if x < 25 {
+            dx = 3
+                
+            }
+            
         
         // Draw an ellipse in the middle of the canvas
         canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
