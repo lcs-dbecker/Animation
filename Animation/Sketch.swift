ef9c2eda321b81d1682d182cbbbdb3b8f0f04900
Dylan Becker <dbecker@lcs-mba3263.local>
2017-10-25 10:54:50 -0400

    Finished first challenge

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,6 +9,9 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     
+    //Change in position
+    var offset : Int
+    
     // This function runs once
     override init() {
         
@@ -18,17 +21,57 @@ class Sketch : NSObject {
         // Set starting position
         x = 250
         
+        // Set difference for x
+        offset = 1
+        
+        // no borders
+        canvas.drawShapesWithBorders = false
+            
+        
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
         // Change position
-        x += 1
+        x += offset
         
-        // Draw an ellipse in the middle of the canvas
+        // Draw fifth ellipse
+        canvas.fillColor = Color.red
+        canvas.drawEllipse(centreX: x, centreY: 450, width: 50, height: 50)
+        
+        
+        
+        // Draw fourth ellipse
+        canvas.fillColor = Color.blue
+         canvas.drawEllipse(centreX: 500 - x, centreY: 350, width: 50, height: 50)
+        
+        
+        // Draw third ellipse
+        canvas.fillColor = Color.green
         canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
         
+        
+        // Draw second ellipse
+        canvas.fillColor = Color.orange
+        canvas.drawEllipse(centreX: 500 - x, centreY: 150, width: 50, height: 50)
+      
+        
+        
+        
+        // Draw first ellipse
+        canvas.fillColor = Color.yellow
+         canvas.drawEllipse(centreX: x, centreY: 50, width: 50, height: 50)
+        if x > 25 {
+            offset = 1
+        }
+        
+      
+            
     }
-    
+
 }
+        
+    
+    
+
