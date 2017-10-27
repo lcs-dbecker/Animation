89b9da283d937da6737d750f9fb88590f51eb840
Dylan Becker <dbecker@lcs-mba3263.lcs.on.ca>
2017-10-27 14:12:55 -0400

    finished the first 2 lines

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,6 +9,10 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     
+    // Change in position
+    var offset : Int
+    
+    
     // This function runs once
     override init() {
         
@@ -18,16 +22,37 @@ class Sketch : NSObject {
         // Set starting position
         x = 250
         
+        // set difference for x and y
+        offset = 3
+        
+        // no borders
+        canvas.drawShapesWithBorders = false
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
+        // change position
+        x += offset
+        
+        // draw first line
+        
+        canvas.fillColor = Color.red
+        canvas.drawEllipse(centreX: x, centreY: x, width: 50, height: 50)
+        
+        //draw second line
+        canvas.fillColor = Color.blue
+        canvas.drawEllipse(centreX:500-x , centreY: 500-x, width: 50, height: 50)
+        
+        //draw third line
+        
+        canvas.fillColor = Color.green
+        canvas.drawEllipse(centreX: 250-x, centreY: 250-x, width: 50, height: 50)
+        
         // Change position
         x += 1
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+  
         
     }
     
