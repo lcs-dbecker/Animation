83be886f2e0cf3991f721eef5d4b3bc2ba98df5c
Dylan Becker <dbecker@lcs-mba3263.lcs.on.ca>
2017-10-31 09:41:10 -0400

    made the dot bounce from top right corner to bottom left corner

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,6 +9,8 @@ class Sketch : NSObject {
     // Position of circle
     var x : Int
     var y : Int
+    var dx :Int
+    var dy :Int
     
     // This function runs once
     override init() {
@@ -21,20 +23,29 @@ class Sketch : NSObject {
         
         //Value for y
         y = 250
-        
-        
+      
+        // Set the starting change
+        dx = 1
+        dy = 1
         
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
+
+        // Change in position
+        x += dx
+        y += dy
         
-        // Change position
-        x += 1
-        
-        //Change position for y
-        y += 1
-        
+        // Make is bounce right
+        if x > 500{
+            dx += -1
+        }
+        //Make it bounce down
+        if y > 500{
+            dy += -1
+        }
+ // make the dot go up and down 
         // Draw background
         canvas.fillColor = Color.white
         canvas.drawRectangle(centreX: 250, centreY: 250, width: 500, height: 500)
