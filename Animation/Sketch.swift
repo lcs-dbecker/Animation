082acfdd2fb5202523fb8637911e1604951c55d0
Dylan Becker <dbecker@lcs-mba3263.lcs.on.ca>
2017-10-31 09:29:28 -0400

    made the dot move in x and y direction

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,6 +8,7 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
+    var y : Int
     
     // This function runs once
     override init() {
@@ -18,6 +19,11 @@ class Sketch : NSObject {
         // Set starting position
         x = 250
         
+        //Value for y
+        y = 250
+        
+        
+        
     }
     
     // Runs in a loop, forever, to create the animated effect
@@ -26,8 +32,17 @@ class Sketch : NSObject {
         // Change position
         x += 1
         
+        //Change position for y
+        y += 1
+        
+        // Draw background
+        canvas.fillColor = Color.white
+        canvas.drawRectangle(centreX: 250, centreY: 250, width: 500, height: 500)
+        
         // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: x, centreY: y, width: 50, height: 50)
+        
         
     }
     
