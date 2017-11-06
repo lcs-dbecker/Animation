50e8c93d894671429ff265cc428585a7372a2e5a
Dylan Becker <dbecker@lcs-mba3263.lcs.on.ca>
2017-11-06 09:38:34 -0500

    Done with cannon ball challenge

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -8,6 +8,7 @@ class Sketch : NSObject {
     
     // Position of circle
     var x : Int
+    var y : Int
     
     // This function runs once
     override init() {
@@ -15,19 +16,29 @@ class Sketch : NSObject {
         // Create canvas object – specify size
         canvas = Canvas(width: 500, height: 500)
         
-        // Set starting position
-        x = 250
+        // Set starting position for x
+        x = 0
+        // Set starting position for y
+        y = 250
         
     }
     
     // Runs in a loop, forever, to create the animated effect
     func draw() {
         
-        // Change position
+        // Change position for x
         x += 1
+        // Change position for y
+        let a = -0.008
+        let h = 250.0
+        let k = 500.0
         
-        // Draw an ellipse in the middle of the canvas
-        canvas.drawEllipse(centreX: x, centreY: 250, width: 50, height: 50)
+        y = Int(a * (Double(x)-h) * (Double(x)-h) + k)
+        
+        
+        // Draw an ellipse in the bottom left of the canvas
+        canvas.drawEllipse(centreX: x, centreY: y, width: 50, height: 50)
+        
         
     }
     
